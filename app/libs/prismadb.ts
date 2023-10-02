98fdc028e3927efa9b3c0af05380913c6de1ea36
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-10-03 02:04:33 +0530

    feat: prisma db schema, mongoDb setup, auth provider additions

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,17 @@
+// Single instance of prisma client in development -> to save multiple database connections on next js HMR
+
+import { PrismaClient } from '@prisma/client';
+
+const prismaClientSingleton = () => new PrismaClient();
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
