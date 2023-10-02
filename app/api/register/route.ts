45b01388dc114590d0b0a4d4be82e85f96fd537e
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-10-03 02:15:47 +0530

    feat: register api

---
+++ app/api/register/route.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/prefer-default-export */
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+import prisma from '@/app/libs/prismadb';
+
+export async function POST(req: Request) {
+  const body = await req.json();
+
+  const { email, name, password } = body;
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword
+    }
+  });
+
+  return NextResponse.json(user);
+}
